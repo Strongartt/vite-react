1299a6c951f59dd9bbd78015d3c1dc4d2bfa77e9
Strongartt <rikardoperez0809@gmail.com>
2025-07-05 14:08:06 -0500

    api rick and morty

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useEffect, useState } from "react";
+import "./App.css";
+export default function App() {
+  type Charactertype={
+    id:string;image:string;name:string;species:string;status:string;
+  } 
+  const [characters, setCharacters] = useState<Charactertype[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => {
+        setCharacters(data.results);      
+        setLoading(false);
+        console.log("hello");
+      })
+      .catch((err) => {
+        console.error("Error al cargar personajes:", err);
+        setLoading(true);
+      });
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    !loading && (
+    <div className="container">
+      <h1>Rick and Morty Viewer</h1>
+      
+        <div className="grid">
+          {characters.map((char) => (
+            <div key={char.id} className="card">
+              <img src={char.image} alt={char.name} />
+              <h3>{char.name}</h3>
+              <p>{char.species} - {char.status}</p>
+            </div>
+          ))}
+        </div>
+    </div>)
+  );
 }
-
-export default App
